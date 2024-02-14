99b872de3969854b7d98dcc56b16f8cf5b5d40c2
Dalei Sun <daleisun@Daleis-MacBook-Pro.local>
2024-02-14 20:42:08 +0800

    feature: customer services

---
+++ testcontainers-demo/src/main/java/com/testcontainers/demo/DBConnectionProvider.java
@@ -0,0 +1,26 @@
+package com.testcontainers.demo;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnectionProvider {
+
+    private final String url;
+    private final String username;
+    private final String password;
+
+    public DBConnectionProvider(String url, String username, String password) {
+        this.url = url;
+        this.username = username;
+        this.password = password;
+    }
+
+    public Connection getConnection() {
+        try {
+            return DriverManager.getConnection(url, username, password);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
